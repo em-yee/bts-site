aed400f8195224cb746ff630700c36eeeb9d7a58
Emily Ye <emily.ye.dev@gmail.com>
2021-10-26 05:50:03 -0400

    updates port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const socket = require('socket.io');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use('/public', express.static(__dirname + '/public'));
 
